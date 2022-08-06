c8912bd407a96747bdb56b269e3924903216e8a1
RanolP <public.ranolp@gmail.com>
2022-08-06 19:10:39 +0900

    forgotten leading slash

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -6,7 +6,7 @@ const typography = css`
     font-style: normal;
     font-weight: normal;
     src: local('Minecraft Regular'),
-      url('fonts/minecraft-regular.woff') format('woff');
+      url('/fonts/minecraft-regular.woff') format('woff');
   }
 
   @font-face {
@@ -14,7 +14,7 @@ const typography = css`
     font-style: italic;
     font-weight: normal;
     src: local('Minecraft Italic'),
-      url('fonts/minecraft-italic.woff') format('woff');
+      url('/fonts/minecraft-italic.woff') format('woff');
   }
 
   @font-face {
@@ -22,7 +22,7 @@ const typography = css`
     font-style: normal;
     font-weight: bold;
     src: local('Minecraft Bold'),
-      url('fonts/minecraft-bold.woff') format('woff');
+      url('/fonts/minecraft-bold.woff') format('woff');
   }
 
   @font-face {
@@ -30,7 +30,7 @@ const typography = css`
     font-style: italic;
     font-weight: bold;
     src: local('Minecraft Bold Italic'),
-      url('fonts/minecraft-bold-italic.woff') format('woff');
+      url('/fonts/minecraft-bold-italic.woff') format('woff');
   }
 
   :global() {
